107a7c90a51ca3ab5c302730f9da9d0517eb846b
jj <alscom27@gmail.com>
2024-10-07 20:32:06 +0900

    config

--- wineshare/src/main/java/com/keduit/wineshare/config/AuditorAwareImpl.java
+++ wineshare/src/main/java/com/keduit/wineshare/config/AuditorAwareImpl.java
@@ -1,22 +1,24 @@
 package com.keduit.wineshare.config;//package com.keduit.wineshare.config;
-//
-//import org.springframework.data.domain.AuditorAware;
-//
-//import java.util.Optional;
-//
-//public class AuditorAwareImpl implements AuditorAware {
-//  @Override
-//  public Optional getCurrentAuditor() {
-//
-////    // 로그인 유저 정보가 담겨있는 객체?
-////    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-////
-////    String userId = "";
-////    if(authentication != null){ //authentication이 널 이아니면 = 로그인 되어있으면
-////      userId = authentication.getName();  //사용자이름을 리턴하게끔
-////    }
-//
-////    return Optional.of(userId);
-//    return null;
-//  }
-//}
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware {
+  @Override
+  public Optional getCurrentAuditor() {
+
+    // 로그인 유저 정보가 담겨있는 객체?
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    String userId = "";
+    if(authentication != null){ //authentication이 널 이아니면 = 로그인 되어있으면
+      userId = authentication.getName();  //사용자이름을 리턴하게끔
+    }
+
+    return Optional.of(userId);
+
+  }
+}
