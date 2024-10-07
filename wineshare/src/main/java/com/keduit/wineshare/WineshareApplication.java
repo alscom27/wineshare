d9fd6593b38f2f83d88a0f558d2aa894ab3d240f
jj <alscom27@gmail.com>
2024-10-07 16:52:54 +0900

    종진복구 한거없음

--- wineshare/src/main/java/com/keduit/wineshare/WineshareApplication.java
+++ wineshare/src/main/java/com/keduit/wineshare/WineshareApplication.java
@@ -1,13 +1,22 @@
 package com.keduit.wineshare;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keduit.wineshare.entity.Wine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class WineshareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WineshareApplication.class, args);
+
 	}
+
 
 }
---
+++ wineshare/src/test/java/com/keduit/wineshare/service/WineServiceTests.java
@@ -0,0 +1,13 @@
+package com.keduit.wineshare.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+public class WineServiceTests {
+
+
+
+}
