a87a7faa72e59eab123d88637d57329aca2691f6
eunwoo <oew0423@gmail.com>
2024-10-07 17:28:26 +0900

    entity name update

---
+++ wineshare/src/main/java/com/keduit/wineshare/controller/WineController.java
@@ -0,0 +1,11 @@
+package com.keduit.wineshare.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wines")
+@RequiredArgsConstructor
+public class WineController {
+}
