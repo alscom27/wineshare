41bf3935db9dba586e3d1db39d0d347516290635
su lim <lucy2092@naver.com>
2024-10-11 11:37:02 +0900

    푸시 테스트

---
+++ wineshare/src/main/java/com/keduit/wineshare/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.keduit.wineshare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping("/")
+  public String main() {
+    return "home1";
+  }
+}
